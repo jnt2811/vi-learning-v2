3e6cd1787b404b010322647dbd67e8cee9a32038
jnt2811 <tranquangnguyen2811@gmail.com>
2022-07-27 23:47:04 +0700

    .

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const bodyParser = require("body-parser");
 
 const cors = require("cors");
 app.use(cors());
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
+const compression = require("compression");
+app.use(compression());
 
 const dotenv = require("dotenv");
 dotenv.config();
